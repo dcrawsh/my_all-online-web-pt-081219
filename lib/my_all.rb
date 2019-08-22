1c5a4269f22887eb423e3cd0c53c713355f996d8
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-22 19:06:47 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,10 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
+block_return_values = []
 i = 0 
 while i < collection.length 
-yield(collection[i])
+block_return_values << yield(collection[i])
 i += 1 
-
+end
+if block_return_values.include?(false)
+  false
+else
+  true
 end
 end
